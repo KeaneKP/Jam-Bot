d92af6201148dc0637e6f201bcb599bc3e8c32d4
KeaneKP <keanekp@k12.ebps.net>
2019-12-10 22:02:18 +0000

    all

--- index.js
+++ index.js
@@ -1,76 +1,94 @@
-/* Setup
-
-   1. Create a .env file (click add file then remane it to .env)
-
-   2. Put "token=" (without quotes) into the .env file followed by your Discord Bot token (No spaces!)
-
-*/
-
-/* If you use uptimerobot to ping, delete this line and line 20
-
-const express = require("express");
-const app = express();
-
-app.listen(() => console.log("Server started"));
-
-app.use('/ping', (req, res) => {
-  res.send(new Date());
-});
-
-*/
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  
+
 });
 
 client.on('message', message => {
-  // Here is where you need to code
   let programmer = message.guild.roles.find(r => r.name === "Programmer");
   let artist = message.guild.roles.find(r => r.name === "Artist");
   let audio = message.guild.roles.find(r => r.name === "Audio");
   let admin = message.guild.roles.find(r => r.name === "admin");
   let writer = message.guild.roles.find(r => r.name === "Writer");
-  if (message.content == "r!add programmer") {
+  if (message.content == "!add programmer") {
     let member = message.member;
     member.addRole(programmer).catch(console.error);
     message.channel.send("The programmer role has been added");
-  } else if (message.content == "r!remove programmer") {
+  }
+  if (message.content == "!remove programmer") {
     let member = message.member;
     member.removeRole(programmer).catch(console.error);
     message.channel.send("The programmer role has been removed");
-  } else if (message.content == "r!add artist") {
+  }
+  if (message.content == "!add artist") {
     let member = message.member;
     member.addRole(artist).catch(console.error);
     message.channel.send("The artist role has been added");
-  } else if (message.content == "r!add audio") {
+  }
+  if (message.content == "!add audio") {
     let member = message.member;
     member.addRole(audio).catch(console.error);
     message.channel.send("The audio role has been added");
-  } else if (message.content == "r!remove artist") {
+  }
+  if (message.content == "!remove artist") {
     let member = message.member;
     member.removeRole(artist).catch(console.error);
     message.channel.send("The artist role has been removed");
-  } else if (message.content == "r!remove audio") {
+  }
+  if (message.content == "!remove audio") {
     let member = message.member;
     member.removeRole(audio).catch(console.error);
     message.channel.send("The audio role has been removed");
-  } else if (message.content == "Who's Joe?") {
+  }
+  if (message.content.toLowerCase().includes("joe") && message.member.toString() != "<@651610055816380442>") {
     message.channel.send("JOE MAMMA");
-  } else if (message.content == "creeper") {
+  }
+  if (message.content.toLowerCase().includes("creeper")) {
     message.channel.send("AWWWWWWWWW MAN");
-  } else if (message.content == "r!add writer") {
+  }
+  if (message.content == "!add writer") {
     let member = message.member;
     member.addRole(writer).catch(console.error);
     message.channel.send("The writer role has been added");
-  } else if (message.content == "r!remove writer") {
+  }
+  if (message.content == "!remove writer") {
     let member = message.member;
     member.removeRole(writer).catch(console.error);
     message.channel.send("The writer role has been removed");
+  }
+
+  if (message.content.toLowerCase().startsWith("!sacrifice ")) {
+    message.channel.send("You have sacrificed " + message.content.slice(11) + sacrificeOutcome())
+  } else if (message.content.toLowerCase().includes("x") && message.member.toString() != "<@651610055816380442>" && message.member.guild.channels.find(r => r.name === "demonstration")) {
+    message.channel.send(message.member.toString() + "  YOU HAVE ANGERED THE SPIRITS! Quick! use !sacrifice *sacrifice* to please them again!");
+  }
+  if (message.content == "Is plague a female") {
+    message.channel.send("Yes, of course.");
+  }
+  if (message.content.startsWith("!roll d")) {
+    if (message.content.includes("+")) {
+      const mod = message.content.indexOf("+");
+      let roll = Math.floor(Math.random() * +message.content.slice(7, mod-1)) + +message.content.slice(mod+1) + 1;
+      message.channel.send("You have rolled a " + roll);
+    } else if (message.content.includes("-")) {
+      const mod = message.content.indexOf("-");
+      let roll = Math.floor(Math.random() * +message.content.slice(7, mod-1)) - +message.content.slice(mod+1) + 1;
+      message.channel.send("You have rolled a " + roll)
+    } else {
+      try {
+        message.channel.send("You have rolled a " + (Math.floor(Math.random() * +message.content.slice(7))+1))
+      } catch {
+        message.channel.send("The command has failed, make sure you are rolling an integer die.")
+      }
+    }
+  }
+  if (message.content.toLowerCase().startsWith("!summon ")) {
+    message.channel.send(message.content.slice(7))
+    message.channel.send(message.content.slice(7))
+    message.channel.send(message.content.slice(7))
   }
 });
 // General = <#600373758221484054>
@@ -82,3 +100,21 @@ client.on('guildMemberAdd', member => {
   })
 });
 client.login(process.env.token);
+
+function sacrificeOutcome() {
+  x = Math.floor(Math.random() * 4);
+  switch (x) {
+    case 0:
+      return ". The spirits are pleased"
+      break;
+    case 1:
+      return ". The spirits are neutral"
+      break;
+    case 2:
+      return ". The spirits are not impressed"
+      break;
+    case 3:
+      return ". The spirits are displeased"
+      break;
+  }
+} 
