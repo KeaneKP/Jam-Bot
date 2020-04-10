05cae2aee569b5fcd3edae3287eff80e83ccf32a
Mokka <mokkamichi.99@gmail.com>
2020-04-10 03:27:35 +0200

    Fix suggestions reactions

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function passiveResponses(client, message) {
 
 		case "server-suggestions":
 			message.react("👍");
-			    .then(() => message.react('👎'))
+			message.react('👎');
 			return true;
 	}
 
